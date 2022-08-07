ab2a9021091e799a3b0a000d1b9eb8f3bee23c94
EVGENIY <parukevich@gmail.com>
2022-08-07 21:11:38 +0300

    feat: inplement function reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let revArrayNumber = String(n).split('').reverse();
+    if (revArrayNumber[revArrayNumber.length - 1] !== '-') {
+       return +revArrayNumber.join('')
+    } else {
+        revArrayNumber.pop();
+        return +revArrayNumber.join('');
+    }    
 }
